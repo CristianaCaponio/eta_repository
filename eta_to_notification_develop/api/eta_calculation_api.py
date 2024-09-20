fbfc7c1fff5fd2603bf639e9c8321c06385855c2
eddi.lorenzi <eddi.lorenzi@ngs-sensors.it>
2024-09-20 09:57:48 +0000

    update db_model

--- eta_to_notification_develop/api/eta_calculation_api.py
+++ eta_to_notification_develop/api/eta_calculation_api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, HTTPException, status
 from model.input_data import InputData
 from utils.eta_calculation_service import TomTomParams
-from model.tomtom_output_data import TomTomResponse
+from model.db_models import TravelData
 from loguru import logger
 import json
 from database.json_model import JsonCollectionTest
@@ -12,52 +12,48 @@ eta_api_router = APIRouter(tags=["Eta-To-Notification"])
 
 app = FastAPI()
 
-#Established connection to MongoDB when app starts
+# Established connection to MongoDB when app starts
+
+
 @app.on_event("startup")
 def startup_db():
-    db_connect()  
-    
-# Included router 
+    db_connect()
+
+
+# Included router
 app.include_router(eta_api_router)
 
-@eta_api_router.post("/eta_to_notification_project/eta_calculation", response_model=TomTomResponse)
-def eta_calculation(input_data: InputData):
-    
+
+@eta_api_router.post("/eta/calculation", response_model=TravelData)
+def eta_calculation(input_data: TravelData):
     try:
-        # Taking delays from JSON file
-        with open("./eta_to_notification_develop/zip_code.json") as cap_file:
-            cap_delays = json.load(cap_file)
-            logger.info(cap_delays)
+        # # Taking delays from JSON file
+        # with open("./eta_to_notification_develop/zip_code.json") as cap_file:
+        #     cap_delays = json.load(cap_file)
+        #     logger.info(cap_delays)
 
-        request_params = TomTomParams.request_params(input_data)
-        logger.info(request_params)
-                        
-        initial_response = TomTomParams.tomtom_request(request_params, input_data)
-       
-        final_response = TomTomParams.calculate_definitive_eta(initial_response, cap_delays)
-                
-        logger.info(final_response)
-        
-        #transforming the json object into a dictionary
-        final_response_dict = final_response.dict()
-        
-        try:
-            # JSON saved into database collection
-            json_document = JsonCollectionTest(response_data=final_response_dict)
-            json_document.save()
-            logger.info("JSON succesfully saved.")
- 
-        except Exception as e:
-            logger.error(f"Error while saving to database: {e}")
+        # request_params = TomTomParams.request_params(input_data)
+        # logger.info(request_params)
 
-        
-        return final_response
-    
+        # initial_response = TomTomParams.tomtom_request(
+        #     request_params, input_data)
+
+        # final_response = TomTomParams.calculate_definitive_eta(
+        #     initial_response, cap_delays)
+
+        # logger.info(final_response)
+
+        # transforming the json object into a dictionary
+        # final_response_dict = final_response.dict()
+
+        logger.info(input_data)
+
     except Exception as ex:
         logger.error(f"An error occurred during ETA calculation: {ex}")
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(ex))
-    
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(ex))
+
+
 @app.on_event("shutdown")
 def shutdown_db():
     db_disconnect()
-    
--- eta_to_notification_develop/model/db_models.py
+++ eta_to_notification_develop/model/db_models.py
@@ -1,16 +1,18 @@
 from pydantic import BaseModel
 from typing import List, Optional
-
+from model.geopy_input_data import GeopyInputData
 """classes for the first input"""
 
+
 class InitialSummary(BaseModel):
-    startAddress: str 
+    startAddress: str
     startLatitude: float
     startLongitude: float
     endAddress: str
     endLatitude: float
     endLongitude: float
-    
+
+
 class InitialStopSummary(BaseModel):
     departureAddress: str
     departureLatitude: float
@@ -19,48 +21,49 @@ class InitialStopSummary(BaseModel):
     arrivalLatitude: float
     arrivalLongitude: float
 
+
 class InitialTravelData(BaseModel):
-    summary: InitialSummary 
-    stops: List[InitialStopSummary] 
+    summary: InitialSummary
+    stops: List[InitialStopSummary]
+
 
 """classes that populate the database"""
-class Summary(BaseModel):    
+
+
+class Summary(BaseModel):
     travelMode: Optional[str] = ""
     lengthInMeters: Optional[int] = 0
     travelTimeInSeconds: Optional[int] = 0
     trafficDelayInSeconds: Optional[int] = 0
     trafficLengthInMeters: Optional[int] = 0
-    startAddress: str
-    startLatitude: float
-    startLongitude: float
-    endAddress: str
+    startAddress: GeopyInputData
+    startLatitude: Optional[float]
+    startLongitude: Optional[float]
+    endAddress:  GeopyInputData
     endLatitude: float
     endLongitude: float
     departureTime: Optional[str] = ""
     arrivalTime: Optional[str] = ""
 
+
 class StopSummary(BaseModel):
     gsin: Optional[str] = ""
     lengthInMeters: Optional[int] = 0
-    travelTimeInSeconds: Optional[int]  = 0
+    travelTimeInSeconds: Optional[int] = 0
     trafficDelayInSeconds: Optional[int] = 0
     trafficLengthInMeters: Optional[int] = 0
-    departureAddress: str
-    departureLatitude: float
-    departureLongitude: float
-    arrivalAddress: str
-    arrivalLatitude: float
-    arrivalLongitude: float
+    departureAddress: GeopyInputData
+    departureLatitude: Optional[float]
+    departureLongitude: Optional[float]
+    arrivalAddress: GeopyInputData
+    arrivalLatitude: Optional[float]
+    arrivalLongitude: Optional[float]
     departureTime: Optional[str] = ""
     arrivalTime: Optional[str] = ""
     delivered: Optional[bool] = False
 
+
 class TravelData(BaseModel):
-    personal_id: int 
-    summary: Summary 
-    stops: List[StopSummary] 
-
-
-
-
-
+    personal_id: str
+    summary: Summary
+    stops: List[StopSummary]
